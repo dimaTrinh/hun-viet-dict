e3c165d81514bb08100c20e1cf3aba716ae7227e
Duc Trinh <duc.trinh.1501@gmail.com>
2020-07-13 19:13:14 +0700

    Updated getEncoding.py, still working on identify why garbage words show up

--- Generating Viet List/getEncoding.py
+++ Generating Viet List/getEncoding.py
@@ -3,7 +3,8 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-def get_encoding(soup):
+#getting the encoding of the site through meta tag
+def get_encoding_by_tag(soup):
     if soup and soup.meta:
         encod = soup.meta.get('charset')
         if encod == None:
@@ -17,31 +18,35 @@ def get_encoding(soup):
                     return "N/A"
     else:
         return "N/A"
-    return encodings
+    return encod
 
 encodings = set()
 
+#getting the encoding by tag or through Unicode Dammit
 for line in sys.stdin:
 	if (line != "" and line != "\n"): #not empty line
 		url = line.strip() #read in each URL from the URL file
+		print(url)
 		try:
 			source = requests.get(url)
 		except:
 			print("Link broken for " + url, file=sys.stderr)
-			continue
+			print("N/A")
 
 
 		try:
 			soup = BeautifulSoup(source.content, 'lxml')
 
-			encoding = get_encoding(soup)
+			encoding = get_encoding_by_tag(soup)
 
 		    #fails to retrieve encoding using tag, try Unicode Dammit
 			if (encoding == "N/A"): 
 				encoding = soup.original_encoding
 
+			print(encoding)
 			encodings.add(encoding)
 		except:
 			print("Can not make soup for " + url, file = sys.stderr)
+			print("N/A")
 
 print(encodings)
