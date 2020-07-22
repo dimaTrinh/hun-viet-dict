f45276ad6cb0821e15c34010e52e71a21acac8d0
Duc Trinh <duc.trinh.1501@gmail.com>
2020-07-22 13:58:19 +0700

    Adding hundict to the repo

---
+++ hundict-master/preprocess.py
@@ -0,0 +1,195 @@
+from optparse import OptionParser
+from collections import defaultdict
+from string import lower 
+
+
+
+def replace_in_input(input_fn, opt):
+    tokens_cnt = [defaultdict(int), defaultdict(int)] # two languages
+    for line in get_tokens(input_fn):
+        for i, tokens in enumerate(line):
+            for t in tokens:
+                tokens_cnt[i][t] += 1
+    named_entities, tokens_cnt = get_named_entities(tokens_cnt, opt)  
+    # strings are in lower form
+
+    if opt.ne_replacement == False:
+        named_entities = ({}, {})
+    
+    tokens_cnt = map_numeric_values(tokens_cnt, opt)
+
+    rare_words = get_rare_words(tokens_cnt, opt)    
+
+    stopwords = get_stopwords(tokens_cnt, opt)
+    
+    if opt.hide_tags:
+        print_with_hidden_tags(input_fn, named_entities, rare_words, stopwords)
+        
+    else:
+        print_with_tags(opt, input_fn, named_entities, rare_words, stopwords)
+
+
+def print_with_tags(opt, input_fn, named_entities, rare_words, stopwords):
+    
+    for line in get_tokens(input_fn):
+        out_str = ''
+        for i, tokens in enumerate(line):
+            for t_ in tokens:
+                t = lower(t_)
+                if t in named_entities[i]:
+                    out_str += ' _NAMED_ENTITY_'
+                elif process_token(t, opt):
+                    out_str += ' _NUMERIC_'
+                elif t in rare_words[i]:
+                    out_str += ' _RARE_'
+                elif t in stopwords[i]:
+                    out_str += ' _STOPWORD_'
+                else:
+                    out_str += ' ' + t
+            out_str += '\t'
+        print out_str[:-1].encode('utf8')
+
+
+def print_with_hidden_tags(input_fn, named_entities, rare_words, stopwords):
+
+    to_be_left_out=(dict(named_entities[0].items()+rare_words[0].items()+stopwords[0].items()),\
+                    dict(named_entities[1].items()+rare_words[1].items()+stopwords[1].items()))
+
+    for line in get_tokens(input_fn):
+         out_str = ''
+         for i, tokens in enumerate(line):
+             for t_ in tokens:
+                 t = lower(t_)
+                 if t not in to_be_left_out[i]:
+                     out_str += ' ' + t
+             out_str += '\t'
+         print out_str[:-1].encode('utf8') 
+
+
+def get_rare_words(tokens_cnt, opt):
+    
+    rare_words =  ({}, {})
+    if opt.rare_threshold == -1:
+        return rare_words
+    else:
+        for i, tok_cnt in enumerate(tokens_cnt):
+            freqs = sorted([(tok, tok_cnt[tok]) for tok in tok_cnt], key=lambda x:x[1], reverse = True)
+            thr_ind, limit_freq = opt.rare_threshold, freqs[min(opt.rare_threshold, len(freqs) - 1)]
+            
+            next_freq = freqs[min(thr_ind + 1, len(freqs) - 1)]
+            while limit_freq == next_freq and thr_ind < len(freqs):
+                thr_ind += 1
+                next_freq = freqs[thr_ind][1]
+            
+            for item in freqs[thr_ind:]:
+                rare_words[i][item[0]] = 1
+    return rare_words               
+
+def get_stopwords(tokens_cnt, opt):
+
+    stopwords = ({}, {})
+    if opt.freq_threshold == -1:
+        return stopwords
+    else:
+        for i, tok_cnt in enumerate(tokens_cnt):
+            freqs = sorted([(tok, tok_cnt[tok]) for tok in tok_cnt], key=lambda x:x[1], reverse = True)
+            thr_ind, limit_freq = opt.freq_threshold, freqs[min(opt.freq_threshold, len(freqs) - 1)]
+
+            next_freq = freqs[min(thr_ind + 1, len(freqs) - 1)]
+            while limit_freq == next_freq and thr_ind < len(freqs):
+                thr_ind += 1
+                next_freq = freqs[thr_ind][1]
+            
+            for item in freqs[:thr_ind]:
+                stopwords[i][item[0]] = 1
+    return stopwords            
+                
+
+def map_numeric_values(tokens_cnt, opt):
+
+    if opt.num_replacement:
+        for tok_cnt in tokens_cnt:
+            keys = tok_cnt.keys()
+            for tok in keys:
+                if process_token(tok, opt):
+                    tok_cnt['_NUMERIC_'] = tok_cnt['_NUMERIC_'] + tok_cnt[tok]
+                    tok_cnt[tok] = 0
+    return tokens_cnt 
+
+def get_named_entities(tokens_cnt, opt):
+    
+
+    named_entities = ({}, {})
+    for i, tok_cnt in enumerate(tokens_cnt):
+            tok_cnt_keys = tok_cnt.keys()
+            for tok in tok_cnt_keys:
+                if not tok.islower() and not lower(tok) == tok:
+                        if lower(tok) not in tok_cnt or float(tok_cnt[tok])/float(tok_cnt[lower(tok)]) > 2:
+                            named_entities[i][lower(tok)] = 1
+                        tok_cnt[lower(tok)] = tok_cnt[lower(tok)] + tok_cnt[tok]
+            for tok in tok_cnt_keys:  
+                if not tok.islower() and not lower(tok) == tok:
+                    tok_cnt[tok] = 0
+    return named_entities, tokens_cnt    
+
+
+def process_token(t_, opt):
+    if opt.num_replacement == False:
+        return False
+    else:
+        string = t_
+        num = False
+        if string.replace(".", "", 1).replace("-", "", 1).isdigit():
+            num = True
+        
+        return num      
+
+
+def get_tokens(fn):
+    f = open(fn)
+    for l in f:
+        if len(l.split('\t')) == 2:
+            s1, s2 = l.split('\t')
+
+            t1 = tokenize(s1.decode('utf8', 'ignore'))
+            t2 = tokenize(s2.decode('utf8', 'ignore'))
+            yield (t1, t2)
+    f.close()
+
+def tokenize(sen):
+    return [i.strip() 
+            for i in sen.strip().split(' ') if i.strip()]
+    
+def create_option_parser():
+
+    parser = OptionParser("usage: %prog [options] input_file")
+
+    parser.add_option('-s', "--size of vocabulary retained", dest="rare_threshold", type="int",
+                      default=-1, help="replace rare words with common symbol")
+
+    parser.add_option('-f', "--freq-based stop word list", dest='freq_threshold', type="int", 
+                      default=-1, help='replace most frequent words woth common symbol')
+
+    parser.add_option('-n', "--numeric-values-replacement", action='store_true', dest='num_replacement',
+                     help="replace numeric strings with common symbol", default=False)
+
+
+    parser.add_option('-c', "--cap/noncap ratio based NE filter", action='store_true', dest='ne_replacement',
+                     help='filter named entites based on ration of capitalized/noncapitalized form', default=False ) 
+
+    parser.add_option('-e', "--hiding tags", action='store_true', dest='hide_tags', 
+                     help='hide tags of filtered tokens', default=False)
+
+
+    return parser
+
+def main():
+    parser = create_option_parser()
+    opt, args = parser.parse_args()
+    input_fn = args[0]
+    replace_in_input(input_fn, opt)
+
+if __name__ == '__main__':
+    main()
+        
+
---
+++ hundict-master/sentence.py
@@ -0,0 +1,88 @@
+class Sentence:
+    def __init__(self, tokens):
+        self._sen = list(tokens)
+
+    def __len__(self):
+        return len(self._sen)
+
+    def __iter__(self):
+        return iter(self._sen)
+
+    def __getitem__(self, key):
+        return self._sen[key]
+
+    def __setitem__(self, key, value):
+        self._sen[key] = value
+
+    def __delitem__(self, key):
+        del self._sen[key]
+
+    def __contains__(self, item):
+        return item in self._sen
+    
+    def __str__(self):
+        return " ".join(self._sen)
+
+    def ngram_positions(self, ngram):
+        result = []
+
+        for starter_index in (i for i, tok in enumerate(self._sen) if tok == ngram[0]):
+            good = True
+            for tok_i, tok in enumerate(ngram[1:]):
+                try:
+                    # search for remaining tokens
+                    if self[starter_index + 1 + tok_i] == tok:
+                        pass
+                    else:
+                        good = False
+                        break
+                except IndexError:
+                    good = False
+                    break
+            if good:
+                result.append(starter_index)
+        return result
+
+    def init_backup(self):
+        if not "_backup_sen" in self.__dict__:
+            self._backup_sen = list(self._sen)
+
+    def remove_ngram(self, ngram, backup=False):
+        positions = self.ngram_positions(ngram)
+        if backup:
+            self.init_backup()
+            # look for position in backup sentence
+            backup_positions = []
+            for pos in positions:
+                backup_pos = 0
+                for i, tok in enumerate(self._backup_sen):
+                    if type(tok) == tuple:
+                        continue
+                    else:
+                        if backup_pos == pos:
+                            backup_positions.append(i)
+                            break
+                        backup_pos += 1
+         
+        for i, pos in enumerate(positions):
+            if backup:
+                for ngram_pos in xrange(len(ngram)):
+                    self._backup_sen[backup_positions[i]+ngram_pos] = (self._backup_sen[backup_positions[i]+ngram_pos],)
+            shift = i * len(ngram)
+            del self._sen[pos-shift:pos-shift+len(ngram)]
+
+    def remove_toks(self, toks, backup=False):
+        if backup:
+            self.init_backup()
+            for i, tok in enumerate(self._sen):
+                if tok in toks:
+                    self._backup_sen[i] = (tok,)
+        self._sen = filter(lambda x: x not in toks, self._sen)
+
+    def get_tokens(self, backup=False):
+        if backup:
+            self.init_backup()
+            return self._backup_sen
+        else:
+            return self._sen
+    
